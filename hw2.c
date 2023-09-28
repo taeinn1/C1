81453dbbd9b58b64809608e26be0c25a5c1572a6
taeinn1 <142794975+taeinn1@users.noreply.github.com>
2023-09-28 15:03:25 +0900

    3주차 과제

---
+++ hw2.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	double km;
+	printf("Please enter kilometers :");
+	scanf("%lf", &km);
+	printf("%.1f km is equal to %.1f miles", km, km / 1.609);
+	return 0;
+
+
+}
